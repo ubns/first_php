f54cd5f8b94283588a774c550f0235c263279476
Node4m <node4m@gmail.com>
2018-05-14 22:31:23 +0900

    first commit 2018-05-14

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP FIRST</title>
+</head>
+<body>
+
+<p>今日は、<?php echo date("Y年m月d"); ?>です。</p>
+
+<p>定数を表す場合は<?php define("teisu", "define")?>"<?php print(teisu)?>"で表す</p>
+
+<p>変数を表す場合は<?php $hensu = "\$変数名"; ?>"<?php print($hensu)?>"で表す</p>
+</body>
+</html>
